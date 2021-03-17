1f07ec1f6fc5a2e3e738d1de59ac7dfbe9192094
Lucy Sheppard <lucysheppard@godatadriven.com>
2021-03-17 17:08:33 +0100

    change to drop

--- answers/cleaning.py
+++ answers/cleaning.py
@@ -1,7 +1,7 @@
 penguins_cleaned = (
     penguins
-    .assign(flipper_length_cm = penguins['flipper_length_cm']*10)
-    .rename(columns = {'flipper_length_cm':'flipper_length_mm'})
+    .assign(flipper_length_mm = penguins['flipper_length_cm']*10)
+    .drop('flipper_length_cm', axis=1)
     .replace('F', 'Female')
     .dropna()
     .set_index('penguin_id')
