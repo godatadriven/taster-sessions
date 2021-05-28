0b37d1c228c81c12b9d92946930271c23bc3b9cf
Lucy Sheppard <lucysheppard@godatadriven.com>
2021-05-28 09:16:22 +0200

    Update answers and intro

--- answers/explore-data.py
+++ answers/explore-data.py
@@ -1,28 +1,33 @@
-# 1. How many missing values are there in the data?
 
-bikes.isnull().sum()
+# 1
 
-
-# 2. What is the earliest and latest date (min/max) in the data?
-
-bikes.index.min(), bikes.index.max()
+print('1. How many missing values are there in the data?',
+      bikes.isnull().sum().sum(), sep='\n')
 
 
-# 3. Which year was the most humid on average (mean)?
+# 2
 
-(
-    bikes
-    .groupby(bikes.index.year)
-    ['temp']
-    .mean()
-)
+print('\n2. What is the earliest and latest date (min/max) in the data?', 
+      f'Min: {bikes.index.min().date()}', f'Max"{bikes.index.max().date()}', sep='\n')
 
-# 4. Plot the amount of bikes (cnt) (Extra: by month) - how does the amount of bikes being rented change between 2011 and 2012?
 
-(
-    bikes['cnt']
-    .groupby([bikes.index.month, bikes.index.year])
-    .sum()
-    .unstack()
-    .plot()
-)
+# 3
+
+print('\n3. Which year was the most humid on average (mean)?', 
+      (
+          bikes
+          .groupby(bikes.index.year)
+          ['temp']
+          .mean()
+      ), sep='\n')
+
+# 4
+
+print('\n4. Plot the amount of bikes (cnt) (Extra: by month) - how does the amount of bikes being rented change between 2011 and 2012?',
+      (
+          bikes['cnt']
+          .groupby([bikes.index.month, bikes.index.year])
+          .sum()
+          .unstack()
+          .plot()
+      ), sep='\n')
